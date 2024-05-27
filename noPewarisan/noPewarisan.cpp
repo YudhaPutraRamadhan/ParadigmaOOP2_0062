0b001613836da5a723f2aff5a9dda60c10af1f3f
MzSuwawa <yudharamadhan838@gmail.com>
2024-05-27 14:59:25 +0700

    Commit masih sama seperti program yang sebelumnya

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -7,3 +7,17 @@ public:
 		cout << "Halo saya Function dari base class";
 	}
 };
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Halo saya Function dari derived class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
